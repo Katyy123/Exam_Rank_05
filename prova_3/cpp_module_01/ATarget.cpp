2083c43579b844524e958a95884b620aed0234bb
Catia Filiberto <cfiliber@e3r3p4.42roma.it>
2023-04-08 19:50:08 +0200

    -

---
+++ prova_3/cpp_module_01/ATarget.cpp
@@ -0,0 +1,36 @@
+#include "ATarget.hpp"
+
+ATarget::ATarget() {
+}
+
+ATarget::ATarget(std::string const & type) : type(type) {
+
+}
+
+ATarget::ATarget(ATarget const & other) {
+
+    *this = other;
+}
+
+ATarget & ATarget::operator=(ATarget const & other) {
+
+    this->type = other.type;
+
+    return (*this);
+}
+
+ATarget::~ATarget() {
+
+}
+
+std::string const & ATarget::getType() const {
+
+    return type;
+}
+
+void ATarget::getHitBySpell(ASpell const & spell) const {
+
+	std::cout << type << " has been " << spell.getEffects() << "!" << std::endl;
+}
+
+
